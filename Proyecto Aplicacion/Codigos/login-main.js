27f7506ae23f664ce0a05c855d86e073f52136ad
LucasSirimarco <99035196+LucasSirimarco@users.noreply.github.com>
2022-07-08 00:14:51 -0300

    Add files via upload
    Aca subo el Login ordenado en carpetas

---
+++ Proyecto Aplicacion/Codigos/login-main.js
@@ -0,0 +1,95 @@
+
+// ------------------------- Codigo para cambiar de Login - Registro -------------------------
+
+function mostrarRegistro(){
+    let estado = document.getElementById("formularioDeRegistro");
+    if (estado.style.display === "block") {
+        estado.style.display = "none";
+        document.getElementById("formularioDeLogueo").style.display = "none";
+    } else {
+        estado.style.display = "block";
+        document.getElementById("formularioDeLogueo").style.display = "none";
+    }
+}
+function mostrarLogin(){
+    let estado = document.getElementById("formularioDeRegistro");
+    if (estado.style.display === "block") {
+        estado.style.display = "none";
+        document.getElementById("formularioDeLogueo").style.display = "block";
+    } else {
+        estado.style.display = "none";
+        document.getElementById("formularioDeLogueo").style.display = "block";
+    }
+}
+function mostrarRegistroDePersona(){
+    let estado = document.getElementById("formularioDeRegistroPersonal");
+    if (estado.style.display === "block") {
+        estado.style.display = "none";
+        document.getElementById("formularioDeRegistro").style.display = "none";
+    } else {
+        estado.style.display = "block";
+        document.getElementById("formularioDeRegistro").style.display = "none";
+    }
+}
+function mostrarRegistroAnterior(){
+    let estado = document.getElementById("formularioDeRegistro");
+    if (estado.style.display === "block") {
+        estado.style.display = "none";
+        document.getElementById("formularioDeRegistroPersonal").style.display = "none";
+    } else {
+        estado.style.display = "block";
+        document.getElementById("formularioDeRegistroPersonal").style.display = "none";
+    }
+}
+
+function mandarDatosLogeo(){
+    let usuario = document.getElementById("Usuario").value;
+    let contraseña = document.getElementById("Contraseña").value;
+    // console.log(usuario + " " + contraseña);
+    // if (usuario === "Lucas" && contraseña === "asdasd123123") {
+    //     window.open("index.html");
+    //     window.close();
+    // } else {
+    //     alert("El Usuario o la Contraseña son invalidas");
+    // }
+}
+
+
+// ------------------------- Codigo para Validar Usuario en Formulario de Logueo-------------------------
+
+document.addEventListener("DOMContentLoaded", function() {  // Cuando escuchamos el evento DOMContentLoaded creamos una funcion que..
+    document.getElementById("formularioDeLogueo").addEventListener('submit', validarFormulario); 
+});// en id del formulario se escucha el evento submit se crea otra funcion 
+
+function validarFormulario(e) {
+    e.preventDefault();
+    let usuario = document.getElementById("Usuario").value;
+
+      if ( usuario == null || usuario.length == 0 || /^\s+$/.test(usuario) ){
+          alert("Escribe un Usuario");
+          return;
+      }
+       else{
+        console.log(usuario);
+        return;
+      }
+
+    
+  }
+
+
+  const soloLetras = (e)=> {
+    let tecla = window.Event ? e.which : e.teclaCode;
+    return ((tecla <= 48 || tecla >= 57)||(tecla == 20) && (tecla !=(tecla <= 91)) )
+}
+//Función que permite solo Números
+// const validarTelefono = (ev)=> {
+//  if ((ev.keyCode < 48) || (ev.keyCode > 57)) 
+//   ev.returnValue = false;
+// }
+// ------------------------- Codigo para Validar Formulario del Usuario -------------------------
+
+document.addEventListener("keydown", (e)=> {
+    let Letra = e.key;
+    
+});
